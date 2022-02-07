3e0b7f5e023b01770bc883a708e720e6b3e63a47
Gabriel Lima <gabrielrtxon@gmail.com>
2022-02-07 14:37:24 -0300

    Better product card component

--- src/Components/Card_Product.jsx
+++ src/Components/Card_Product.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Styles/Card_Product.css";
 
-function Card_Product() {
+function Card_Product({
+  nome = "Conjunto Necessaire",
+  preço = "99",
+  imgLink = "/Imgs/test.webp",
+  especial = true,
+  textoEspecial = "NOVIDADE",
+}) {
   return (
     <div className="card__product">
-      <img src="/Imgs/test.webp" alt="produto" className="card__product__img" />
+      <div className="card__product__imgContainer">
+        <img src={imgLink} alt={nome} />
+        {especial && <p>{textoEspecial}</p>}
+      </div>
+
       <div className="card__product__texts">
-        <p className="card__product__texts__name">Bolsa Escolar Grande</p>
-        <h3 className="card__product__texts__price">R$ 120,00</h3>
+        <p className="card__product__texts__name">{nome}</p>
+        <h3 className="card__product__texts__price">R$ {preço}</h3>
         <button className="card__product__texts__btn">VER PRODUTO</button>
       </div>
     </div>

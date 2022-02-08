533b53febd41171dba865a46a3f9bc3902b73f9b
Gabriel Lima <gabrielrtxon@gmail.com>
2022-02-08 19:11:21 -0300

    Added vestuario filter button

--- src/Pages/Vestuário.js
+++ src/Pages/Vestuário.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../Components/Styles/PagesCSS/Vestuario.css";
 import Navbar from "../Components/Navbar";
@@ -6,6 +6,7 @@ import Footer from "../Components/Footer";
 import Nav from "../Components/Nav";
 
 function Vestuário() {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
   return (
     <div className="vestuário">
       <Navbar />
@@ -21,6 +22,38 @@ function Vestuário() {
             { nome: "Vestuário", link: "/vestuario" },
           ]}
         />
+
+        <div className="vestuário__content__filtro">
+          <button
+            className="vestuário__content__filtro__btn"
+            onClick={() => setIsFilterOpen(!isFilterOpen)}
+          >
+            ORGANIZAR POR{" "}
+            <i
+              className={`fal fa-angle-down filterArrow ${
+                isFilterOpen ? "filterArrowRot" : null
+              }`}
+            ></i>
+          </button>
+          <div
+            class={`vestuário__content__filtro__dropdown ${
+              isFilterOpen ? "filterOpen" : null
+            }`}
+          >
+            <button className="vestuário__content__filtro__dropdown__item">
+              Menor preço
+            </button>
+            <button className="vestuário__content__filtro__dropdown__item">
+              Maior Preço
+            </button>
+            <button className="vestuário__content__filtro__dropdown__item">
+              A-Z
+            </button>
+            <button className="vestuário__content__filtro__dropdown__item">
+              Z-A
+            </button>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>

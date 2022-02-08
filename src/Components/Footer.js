11971d8029d353dd17ea7670f3f90a79c115a173
Gabriel Lima <gabrielrtxon@gmail.com>
2022-02-07 22:09:21 -0300

    Added footer third column

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -67,7 +67,9 @@ function Footer() {
           <ul>
             {c1Items.map((item, index) => (
               <li className="footer__content__column1__item" key={index}>
-                <Link to={item.link}>{item.text}</Link>
+                <Link className="footLink" to={item.link}>
+                  {item.text}
+                </Link>
               </li>
             ))}
           </ul>
@@ -85,11 +87,61 @@ function Footer() {
           <h3 className="footer__topicTitle">Linhas de Produto</h3>
           <ul>
             {c2Items.map((item, index) => (
-              <li className="footer__content__column2__item" key={index}>
-                <Link to={item.link}>{item.text}</Link>
+              <li className="footer__content__column2__item " key={index}>
+                <Link className="footLink" to={item.link}>
+                  {item.text}
+                </Link>
               </li>
             ))}
           </ul>
+        </div>
+
+        <div className="footer__content__column3">
+          <div className="footer__content__column3__phone">
+            <i className="fal fa-phone-alt"></i>
+            <a href="tel:+555133203500" className="footLink">
+              (51) 3320-3500
+            </a>
+          </div>
+
+          <div className="footer__content__column3__email">
+            <i className="fal fa-envelope"></i>
+            <a href="mailto:pucrsstore@pucrs.br" className="footLink">
+              pucrsstore@pucrs.br
+            </a>
+          </div>
+
+          <div className="footer__content__column3__texts">
+            <h4>Horário de atendimento:</h4>
+            <p>
+              Atendimento presencial de segunda a sexta-feira, das 9h às
+              21h30min.
+              <br /> Para maiores informações entre em contato pelo e-mail&nbsp;
+              <a href="mailto:pucrsstore@pucrs.br" className="footLink2">
+                pucrsstore@pucrs.br
+              </a>
+              , pelo telefone&nbsp;
+              <a href="tel:+555133534860" className="footLink2">
+                (51) 3353-4860
+              </a>
+              &nbsp;ou pelo WhatsApp&nbsp;
+              <a href="tel:+5551996884990" className="footLink2">
+                (51) 99688-4990
+              </a>
+              . Os produtos da loja também podem ser adquiridos através do site
+              ou WhatsApp.
+            </p>
+            <p className="footer__content__column3__texts__map">
+              <a
+                className="footLink2"
+                href="https://www.google.com/maps/place/Living+360°/@-30.0590135,-51.1762396,17z/data=!4m8!1m2!2m1!1sLiving+360°!3m4!1s0x95197729bdae1b19:0xc9dad1213413cf45!8m2!3d-30.0590419!4d-51.1740509"
+                target="_blank"
+              >
+                Av. Ipiranga, 6681 - Porto Alegre/RS Living 360° (Prédio 15) -
+                Térreo
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </div>

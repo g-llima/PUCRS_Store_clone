68aea1675813403940b7bd5ef18ee8d9134439ca
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-28 14:11:26 -0300

    Code for search products

--- src/Pages/Buscar.js
+++ src/Pages/Buscar.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import "../Components/Styles/PagesCSS/Buscar.css";
+import { ProductContext } from "../ProductContext";
 import Nav from "../Components/Nav.jsx";
 
 function Buscar() {
+  const [products, setProducts] = useState([]);
+
+  const { contextValue } = useContext(ProductContext);
+
+  let tempProducts = [];
+  useEffect(() => {
+    contextValue.map((item) => {
+      if (item["name"].toLowerCase().includes("conjunto")) {
+        tempProducts.push(item);
+      }
+    });
+    setProducts(tempProducts);
+  }, [contextValue]);
+
+  console.log(products);
+
   return (
     <div className="search">
       <Nav
